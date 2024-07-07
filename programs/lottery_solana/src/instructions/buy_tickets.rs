e463422abbc35120001b29f8e912808038135ee7
Patrick <patrick@emotiv.com>
2024-07-07 23:40:47 +0800

    Added player stats in buy ticket

--- programs/lottery_solana/src/instructions/buy_tickets.rs
+++ programs/lottery_solana/src/instructions/buy_tickets.rs
@@ -2,6 +2,7 @@ use anchor_lang::prelude::*;
 use crate::{
     state::Game,
     state::Ticket,
+    state::Player,
     error::ErrorCode,
     TICKET_PRICE,
     _has_game_ended};
@@ -11,6 +12,7 @@ pub fn _buy_ticket(ctx: Context<BuyTicket>) -> Result<()> {
     let player = &ctx.accounts.player;
     let game   = &mut ctx.accounts.game;
     let ticket = &mut ctx.accounts.ticket;
+    let player_stats = &mut ctx.accounts.player_stats;
 
     // Check if the game has ended, players cannot buy tickets after the game ends
     require!(_has_game_ended(game) == false, ErrorCode::GameEnded);
@@ -42,6 +44,11 @@ pub fn _buy_ticket(ctx: Context<BuyTicket>) -> Result<()> {
     game.prize_pool += TICKET_PRICE;
     game.total_tickets += 1;
 
+    // Update the player stats
+    player_stats.player = player.key();
+    player_stats.amount += TICKET_PRICE;
+    player_stats.ticket_count += 1;
+
     Ok(())
 }
 
@@ -50,8 +57,8 @@ pub struct BuyTicket<'info> {
     #[account(
         init, 
         seeds = [
-            &game.total_tickets.to_be_bytes(),
-            game.key().as_ref()
+            game.key().as_ref(),
+            &game.total_tickets.to_be_bytes()
         ], 
         constraint = player.to_account_info().lamports() >= TICKET_PRICE,
         bump, 
@@ -59,6 +66,19 @@ pub struct BuyTicket<'info> {
         space = 32 + 4 + 8,
     )]
     pub ticket: Account<'info, Ticket>,
+
+    #[account(
+        init,
+        seeds = [
+            game.key().as_ref(),
+            player.key().as_ref()
+        ],
+        bump,
+        payer = player,
+        space = 32 + 8 + 4 + 8,
+    )]
+    pub player_stats: Account<'info, Player>,
+
     #[account(mut)]
     pub player: Signer<'info>,
     #[account(mut)]
