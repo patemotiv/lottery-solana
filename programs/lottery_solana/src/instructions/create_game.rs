cabf2e41ca684021c4963e6090f3f868e996b70a
YitJian <yjwong@LAPTOP-MV5P8M4N>
2024-07-09 19:15:36 +0800

    Implemented create_game

--- programs/lottery_solana/src/instructions/create_game.rs
+++ programs/lottery_solana/src/instructions/create_game.rs
@@ -2,10 +2,24 @@ use anchor_lang::prelude::*;
 use crate::{
     state::Game,
     GAME_ACCOUNT_SEED,
+    error::ErrorCode,
+    is_end_time_valid
 };
 
-pub fn _create_game(_ctx: Context<CreateGame>, _end_time: i64) -> Result<()> {
-    // TBD
+pub fn _create_game(ctx: Context<CreateGame>, end_time: i64) -> Result<()> {
+    let game = &mut ctx.accounts.game;
+    let creator = &ctx.accounts.creator;
+
+    require!(is_end_time_valid(end_time) == true, ErrorCode::InvalidGameEndTime);
+
+    // Initialize Game account fields
+    game.creator = creator.key();
+    game.end_time = end_time;
+    game.prize_pool = 0; // Start with an empty prize pool
+    game.total_tickets = 0; // No tickets sold initially
+    game.winner = None;
+    game.winner_withdrawn = false;
+
     Ok(())
 }
 
--- programs/lottery_solana/src/state/lottery_solana.rs
+++ programs/lottery_solana/src/state/lottery_solana.rs
@@ -2,7 +2,7 @@ use anchor_lang::prelude::*;
 
 #[account]
 pub struct Game {
-    pub owner: Pubkey, // 32
+    pub creator: Pubkey, // 32
     pub end_time: i64,  // 8
     pub prize_pool: u64, // 8
     pub total_tickets: u32, // 4
@@ -27,4 +27,10 @@ pub struct Player {
 pub fn _has_game_ended(game: &Game) -> bool {
     let clock = Clock::get().unwrap();
     (game.end_time == 0) || (clock.unix_timestamp >= game.end_time)
+}
+
+pub fn is_end_time_valid(end_time : i64) -> bool {
+    // Check if the provided end_time is in the future
+    let clock = Clock::get().unwrap();
+    end_time > clock.unix_timestamp 
 }
